0f2a428a05de34ced8d24205c389ec4be1495d12
alexdsouzaf <81589945+alexdsouzaf@users.noreply.github.com>
2022-03-20 16:39:20 -0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using System.Threading.Tasks;
+using System.IO;
+using ToDoXamarin.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoXamarin.Droid
+{
+    [Activity(Label = "ToDoXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            #region[DATABASE]
+
+            var contexto = new MyDBContext();
+            contexto.CreateDataBaseEF();
+            
+            #endregion
+
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }        
+    }
+}
